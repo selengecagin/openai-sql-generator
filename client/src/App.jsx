76c43558bdca2d112a0cf9639e219e4966ad7f7f
selengecagin <selengecagin@gmail.com>
2024-08-28 12:53:06 +0300

    Add input field in form for query description

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,16 @@ function App() {
   return (
     <main className={styles.main}>
       <img src={sqlLogo} alt="sql-logo" className={styles.icon} />
+      <h3>Generate SQL with AI</h3>
+
+      <form>
+        <input
+          type="text"
+          name="quesy-description"
+          placeholder="Describe your query"
+        />
+        
+      </form>
     </main>
   );
 }
