157980175d0058dd59db0ae57b1e31bc3dcca189
selengecagin <selengecagin@gmail.com>
2024-08-28 14:10:28 +0300

    Add root endpoint and return JSON response

--- server/index.js
+++ server/index.js
@@ -7,3 +7,8 @@ const app = express();
 app.use(cors());
 
 const port = process.env.PORT || 3005;
+
+// create endpoints
+app.get("/",(req,res)=>{
+    res.json({ status: "operational", message: "API is up and running" });
+})
