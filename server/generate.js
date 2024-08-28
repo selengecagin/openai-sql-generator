cc2ce4fa5c0ccc26c7a77085e513445e8c5f1113
selengecagin <selengecagin@gmail.com>
2024-08-28 16:46:52 +0300

    Integrate OpenAI DaVinci model for SQL query generation

--- server/generate.js
+++ server/generate.js
@@ -1,14 +1,18 @@
 import openaiClient from "./api.js";
 
-// SQL query:\n\n${queryDescription}. -> '.' 
+// SQL query:\n\n${queryDescription}. -> '.'
 const generate = async (queryDescription) => {
-  const response = await openaiClient.createCompletion({
-    model: "text-davinci-003",
-    prompt: `Convert the following natural language description into a SQL query:\n\n${queryDescription}.`,
-    max_tokens: 100, // max length of the res
-    temperature: 0, //0 to 10 (0 means no creativity, 10 means high creativity) - 0 return exactly same completion every time - 10 generates brand names, pet names etc.
-  })
-  return response.data.choices[0].text
+  const daVinci = async (queryDescription) => {
+    const response = await openaiClient.createCompletion({
+      model: "text-davinci-003", // text-complation api
+      prompt: `Convert the following natural language description into a SQL query:\n\n${queryDescription}.`,
+      max_tokens: 100, // max length of the res
+      temperature: 0, //0 to 10 (0 means no creativity, 10 means high creativity) - 0 return exactly same completion every time - 10 generates brand names, pet names etc.
+    });
+    return response.data.choices[0].text;
+  };
+
+  return await daVinci(queryDescription);
 };
 
-export default generate
+export default generate;
