3ab28d9668473158aa820e35c376cb80770792f8
selengecagin <selengecagin@gmail.com>
2024-08-28 12:57:13 +0300

    Initialize queryDescription state for user input

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import styles from "./index.module.css";
 import sqlLogo from "./assets/sql-logo.png";
+import { useState } from "react";
 
 function App() {
+  //queryDescription = input = user prompt
+  const [queryDescription, setQueryDescription] = useState("");
+
   return (
     <main className={styles.main}>
       <img src={sqlLogo} alt="sql-logo" className={styles.icon} />
