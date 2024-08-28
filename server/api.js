775f27dbba8ba42afc69d3880a7a786f0a4fdc4a
selengecagin <selengecagin@gmail.com>
2024-08-28 13:51:08 +0300

    Set up the configuration of Open AI Client

--- server/api.js
+++ server/api.js
@@ -4,10 +4,15 @@ import { Configuration, OpenAIApi } from "openai";
 import dotenv from "dotenv";
 dotenv.config();
 
-const openaiApiKey = process.env.OPENAI_API_KEY;
+const openAiApiKey = process.env.OPENAI_API_KEY;
 // To access on the info in the .env file - process.env.nameofvariable
 
 if (!openaiApiKey) {
   console.error("OPENAI_API_KEY is not set!");
   process.exit(1);
 }
+
+// Set up the configuration of Open AI Client
+const configuration = new Configuration({
+  apiKey: openAiApiKey,
+});
