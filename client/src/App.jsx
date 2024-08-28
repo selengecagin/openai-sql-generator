00f999b97447c4e2f47914bded73756575f0d471
selengecagin <selengecagin@gmail.com>
2024-08-28 13:16:26 +0300

    Handle form submission

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,23 @@ function App() {
   //queryDescription = input = user prompt
   const [queryDescription, setQueryDescription] = useState("");
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <main className={styles.main}>
       <img src={sqlLogo} alt="sql-logo" className={styles.icon} />
       <h3>Generate SQL with AI</h3>
 
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           name="quesy-description"
           placeholder="Describe your query"
-          onChange={(e)=>{setQueryDescription(e.target.value)}}
+          onChange={(e) => {
+            setQueryDescription(e.target.value);
+          }}
         />
         <input type="submit" value="Generate" />
       </form>
