5709996e6a0911d899c3a1a446e8e420c57a2fa0
selengecagin <selengecagin@gmail.com>
2024-08-28 12:50:43 +0300

    Display SQL logo in React component with CSS styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import styles from "./index.module.css";
+import sqlLogo from "./assets/sql-logo.png";
 
 function App() {
-
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <img src={sqlLogo} alt="sql-logo" className={styles.icon} />
+    </main>
+  );
 }
 
 export default App;
